95f8790fdf300d383cd170c766b35b008b2c57f3
sbx_user1051 <sbx_user1051@169.254.83.109>
2024-04-16 17:36:48 +0000

    Tue Apr 16 17:36:48 UTC 2024

---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,9 @@
+from solutions.CHK import checkout_solution
+
+
+class TestCheckout():
+    def test_checkout(self):
+        assert checkout_solution.checkout("") == 
+
+    def test_checkout2(self):
+        assert checkout_solution.checkout("") == 
--- test/solution_tests/HLO/test_sum.py
+++
@@ -1,9 +0,0 @@
-from solutions.HLO import hello_solution
-
-
-class TestHello():
-    def test_hello(self):
-        assert hello_solution.hello("John") == "Hello, John!"
-
-    def test_hello2(self):
-        assert hello_solution.hello("Chris") == "Hello, Chris!"
