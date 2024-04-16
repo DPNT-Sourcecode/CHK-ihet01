3cc4a176febe884080e5d41d936be6400cc17057
sbx_user1051 <sbx_user1051@169.254.83.109>
2024-04-16 19:18:00 +0000

    Tue Apr 16 19:18:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -29,17 +29,19 @@ def checkout(skus):
         if not sku in supermarket_stock:
             return -1
         
+        total_item_price = 0
         rem_quantity = quantity
-        # Starting from the offer with the higher quantity as it is already sorted by descending
-        for offer in supermarket_stock[sku].offers:
-            if offer:
-                no_offer_claimed = quantity // offer.quantity
-                rem_quantity = quantity % offer.quantity
-                total_item_price = (no_offer_claimed * offer.price) + (rem_quantity * supermarket_stock[sku].price)
-            else:
-                total_item_price = quantity * supermarket_stock[sku].price
+        if supermarket_stock[sku].offers:
+            # Starting from the offer with the higher quantity as it is already sorted by descending
+            for offer in supermarket_stock[sku].offers:
+                no_offer_claimed = rem_quantity // offer.quantity
+                rem_quantity = rem_quantity % offer.quantity
+                total_item_price += (no_offer_claimed * offer.price)
+
+        total_item_price += rem_quantity * supermarket_stock[sku].price
 
         total_price += total_item_price
 
     return total_price
+
 
