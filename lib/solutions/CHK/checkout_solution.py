bab4ef44b0385664541bcda59be4ed49af3c844b
sbx_user1051 <sbx_user1051@169.254.83.109>
2024-04-16 18:30:00 +0000

    Tue Apr 16 18:30:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,6 @@
 from collections import Counter
-import supermarket_stock_prices
-
+from . import supermarket_stock_prices
+from supermarket_stock_prices import supermarket_stock
 
 # noinspection PyUnusedLocal
 # skus = unicode string
