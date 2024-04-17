c789e731e06999a4648d2ab758e491de034f9ab6
sbx_user1051 <sbx_user1051@169.254.40.181>
2024-04-17 16:41:09 +0000

    Wed Apr 17 16:41:09 UTC 2024

--- lib/solutions/CHK/supermarket_stock_prices.py
+++ lib/solutions/CHK/supermarket_stock_prices.py
@@ -1,40 +1,17 @@
 class StoreItem:
-    def __init__(self, name, price, offers=None):
+    def __init__(self, name, price):
         if not (isinstance(name, str) and len(name) == 1 and isinstance(price, int) and (isinstance(offers, list) or offers is None)):
                 raise TypeError("StoreItem class accepts a string of one letter for item name, an integer for item price and \
                                  optional offers list of type list of list with 2 integers [[int, int]].")
         
         self.name = name
         self.price = price
-        self.offers = None
-        # if offers:
-        #     self.offers = []
-        #     for offer in offers:
-        #         self.offers.append(self.Offer(offer))
-            
-        #     self.offers = sorted(self.offers, key=lambda x: x.quantity, reverse=True)
 
     def __str__(self):
         string = f"Item: {self.name} | Price: {self.price}"
-        # if self.offers:
-        #     string += f" | Offers: {[str(offer) for offer in self.offers]}"
+
 
         return string
-
-    # class Offer:
-    #     def __init__(self, details):
-    #         length_condition = (len(details) == 2) or (len(details) == 3 and isinstance(details[2], str))
-    #         if not (isinstance(details, list) and length_condition and isinstance(details[0], int) and isinstance(details[1], int)):
-    #             raise TypeError("Offer class accepts EITHER: \n  1. a list of two integers ([int, int]) where the first element represents \
-    #                             the quantity of the item and the second one the total offer price like [3, 150] \
-    #                             \nOR \n  2. a list of two integers and one string ([int, int, str]) where the first element represents \
-    #                             the quantity of the item, the second one is the quantity of the free item and the third one is the free item like [2, 1, 'B'].")
-
-    #         self.quantity = details[0]
-    #         self.price = details[1]
-
-    #     def __str__(self):
-    #         return f"{self.quantity} for {self.price}"
 
 
 class Offer:
@@ -67,25 +44,28 @@ class Offer:
 
 
 supermarket_stock = {
-    'A': StoreItem('A', 50, [[3, 130], [5, 200]]),
-    'B': StoreItem('B', 30, [[2, 45]]),
+    'A': StoreItem('A', 50),
+    'B': StoreItem('B', 30),
     'C': StoreItem('C', 20),
     'D': StoreItem('D', 15),
-    'E': StoreItem('E', 40, [[2, 1, 'B']]),
+    'E': StoreItem('E', 40),
+    'F': StoreItem('F', 10)
 }
 
 offers = [
     Offer('A', 3, price=130),
     Offer('A', 5, price=200),
     Offer('B', 2, price=45),
-    Offer('E', 2, free_quantity=1, free_item='B')
+    Offer('E', 2, free_quantity=1, free_item='B'),
+    Offer('F', 2, free_quantity=1, free_item='F')
 ]
 
 # offers_priority = sorted(offers, key=lambda x: (x.type, -x.quantity))
 offers_priority = sorted(offers, key=lambda x: x.discounted_price, reverse=True)    # Based on the note that all offers are well-balanced (a bit unclear, but assuming)
 
-for item in supermarket_stock.values():
-    print(item)
 
-for offer in offers_priority:
-    print(offer)
+# for item in supermarket_stock.values():
+#     print(item)
+
+# for offer in offers_priority:
+#     print(offer)
