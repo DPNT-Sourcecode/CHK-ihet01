58ca4481e3df8a424c2adf0f5f84dae26370d998
sbx_user1051 <sbx_user1051@169.254.40.181>
2024-04-17 15:54:51 +0000

    Wed Apr 17 15:54:51 UTC 2024

--- lib/solutions/CHK/supermarket_stock_prices.py
+++ lib/solutions/CHK/supermarket_stock_prices.py
@@ -87,5 +87,5 @@ offers_priority = sorted(offers, key=lambda x: x.discounted_price, reverse=True)
 for item in supermarket_stock.values():
     print(item)
 
-for offer in offers:
+for offer in offers_priority:
     print(offer)
